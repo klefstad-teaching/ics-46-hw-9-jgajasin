51098a45cb9465b7c2ffdf30cd8796ae0fee3a82
jgajasin <jgajasin@uci.edu>
2025-03-15 16:39:44 +0000

    Commit of HW9.

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -9,9 +9,9 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     distances[source] = 0;
     previous[source] = -1;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-    minHeap.push({source, 0});
+    minHeap.push({0, source});
     while (!minHeap.empty()){
-        int u = minHeap.top().first;
+        int u = minHeap.top().second;
         minHeap.pop();
         if (visited[u]) continue;
         visited[u] = true;
@@ -21,7 +21,7 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
             if (distances[u] + weight < distances[v]){
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
-                minHeap.push({v, distances[v]});
+                minHeap.push({distances[v], v});
             }
         }
     } 
